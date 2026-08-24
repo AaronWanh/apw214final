a4e870a7b9f9d99fb5d9af3ea490ce0ffe4af8d3
AaronWanh <aaronparsonwang2004@gmail.com>
2026-08-24 11:57:25 -0700

    adding data

---
+++ dataexploration.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+bq1 = read_csv("data/QuebradaCuenca1-Bisley.csv")
+bq2 = read_csv("data/QuebradaCuenca2-Bisley.csv")
+bq3 = read_csv("data/QuebradaCuenca3-Bisley.csv")
+prm = read_csv("data/RioMameyesPuenteRoto.csv")
